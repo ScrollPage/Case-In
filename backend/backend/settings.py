2ea695b7ac244d0d9836a76a3554ab62c9dbc67a
Michail Gurbanov <54814200+reqww@users.noreply.github.com>
2021-04-12 11:54:56 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,23 +119,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('SQL_ENGINE', local.SQL_ENGINE),
-        'NAME': os.environ.get('SQL_DATABASE', local.SQL_DATABASE),
-        'USER': os.environ.get('SQL_USER', local.SQL_USER),
-        'PASSWORD': os.environ.get('SQL_PASSWORD', local.SQL_PASSWORD),
-        'HOST': os.environ.get('SQL_HOST', local.SQL_HOST),
-        'PORT': os.environ.get('SQL_PORT', local.SQL_PORT),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.environ.get('SQL_ENGINE', local.SQL_ENGINE),
+#         'NAME': os.environ.get('SQL_DATABASE', local.SQL_DATABASE),
+#         'USER': os.environ.get('SQL_USER', local.SQL_USER),
+#         'PASSWORD': os.environ.get('SQL_PASSWORD', local.SQL_PASSWORD),
+#         'HOST': os.environ.get('SQL_HOST', local.SQL_HOST),
+#         'PORT': os.environ.get('SQL_PORT', local.SQL_PORT),
+#     }
+# }
 
 
 # Password validation
